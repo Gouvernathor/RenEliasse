b01951b7218ca5a1b8ca1f7a43d67f7f48654bbd
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2023-11-30 23:06:15 +0100

    Make the utils renew the list/dict entirely

--- game/utils_ren.py
+++ game/utils_ren.py
@@ -1,8 +1,8 @@
-import renpy
+import renpy # type: ignore
+
 """renpy
 init python:
 """
-
 # make a decorator version ? meh, not picklable...
 def a_wait_list(func, *args, restart_interaction=True, **kwargs):
     """
@@ -12,18 +12,17 @@ def a_wait_list(func, *args, restart_interaction=True, **kwargs):
     and restarts the interaction (for a use in screens).
     """
     rv = []
-
     def threaded():
         """
         Calls the function, collects the return value,
         lodges the side-effect update in the main thread.
         """
         late_rv = func(*args, **kwargs)
-
-        renpy.invoke_in_main_thread(rv.extend, late_rv)
-        if restart_interaction:
-            renpy.invoke_in_main_thread(renpy.restart_interaction)
-
+        def unthreaded():
+            rv[:] = late_rv
+            if restart_interaction:
+                renpy.restart_interaction()
+        renpy.invoke_in_main_thread(unthreaded)
     renpy.invoke_in_thread(threaded)
     return rv
 
@@ -31,8 +30,11 @@ def a_wait_dict(func, *args, restart_interaction=True, **kwargs):
     rv = {}
     def threaded():
         late_rv = func(*args, **kwargs)
-        renpy.invoke_in_main_thread(rv.update, late_rv)
-        if restart_interaction:
-            renpy.invoke_in_main_thread(renpy.restart_interaction)
+        def unthreaded():
+            rv.clear()
+            rv.update(late_rv)
+            if restart_interaction:
+                renpy.restart_interaction()
+        renpy.invoke_in_main_thread(unthreaded)
     renpy.invoke_in_thread(threaded)
     return rv
