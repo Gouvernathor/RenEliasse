5872e4b75007fe18618163079924b8b4969ae38e
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2023-11-30 23:51:27 +0100

    Ajout de error_rv et de default_rv

--- game/utils_ren.py
+++ game/utils_ren.py
@@ -1,40 +1,69 @@
 import renpy # type: ignore
+python_object = object
 
 """renpy
 init python:
 """
-# make a decorator version ? meh, not picklable...
-def a_wait_list(func, *args, restart_interaction=True, **kwargs):
+
+__notpassed = python_object()
+
+def a_wait_list(func, *, default_rv=__notpassed, error_rv, restart_interaction=True):
     """
     Takes a callable function which returns a list but takes time.
     Returns immediately an empty list and schedules the function to populate it.
     When the "populator" function returns, side-effects it to the previously returned list,
     and restarts the interaction (for a use in screens).
+
+    default_rv and error_rv should be iterables.
     """
-    rv = []
+
+    if default_rv is __notpassed:
+        rv = []
+    else:
+        rv = list(default_rv)
+
     def threaded():
         """
         Calls the function, collects the return value,
         lodges the side-effect update in the main thread.
         """
-        late_rv = func(*args, **kwargs)
+
+        try:
+            late_rv = func()
+        except Exception:
+            late_rv = error_rv
+
         def unthreaded():
             rv[:] = late_rv
+
             if restart_interaction:
                 renpy.restart_interaction()
+
         renpy.invoke_in_main_thread(unthreaded)
+
     renpy.invoke_in_thread(threaded)
     return rv
 
-def a_wait_dict(func, *args, restart_interaction=True, **kwargs):
-    rv = {}
+def a_wait_dict(func, *, default_rv=__notpassed, error_rv, restart_interaction=True):
+    if default_rv is __notpassed:
+        rv = {}
+    else:
+        rv = dict(default_rv)
+
     def threaded():
-        late_rv = func(*args, **kwargs)
+        try:
+            late_rv = func()
+        except Exception:
+            late_rv = error_rv
+
         def unthreaded():
             rv.clear()
             rv.update(late_rv)
+
             if restart_interaction:
                 renpy.restart_interaction()
+
         renpy.invoke_in_main_thread(unthreaded)
+
     renpy.invoke_in_thread(threaded)
     return rv
